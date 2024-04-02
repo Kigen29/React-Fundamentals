80b15aae7d7a1e74abc0bc952e082debcbcc1483
Kigen29 <emmanuel.kemboi@strathmore.edu>
2024-04-02 20:22:45 +0300

    feat: add Task 1 - Rendering Lists

--- src/components/Foodblog.js
+++ src/components/Foodblog.js
@@ -1,6 +1,7 @@
 import './Style.css'
 import React from "react";
 import ProfileName from './ProfileName';
+import OtherBlogs from './OtherBlogs';
 
 function Foodblog() {
   return (
@@ -8,6 +9,8 @@ function Foodblog() {
     <h1 className="item">My Food Blog</h1>
       <ProfileName />
       <p>My favorite food is pizza.</p>
+      <OtherBlogs />
+      
     </div>
   );
 }
---
+++ src/components/OtherBlogs.js
@@ -0,0 +1,34 @@
+import './Style.css'
+import React from "react";
+
+const blogs = [
+
+    { title: 'Love For Food', id: 1 },
+    { title: 'Fruits You Should Buy', id: 2 },
+    { title: 'My Journey To Being a Chef', id: 3 },
+];
+
+function OtherBlogs() {
+
+    const blogList = blogs.map((blog) => 
+    <li
+    key={blog.id}
+    >
+    {blog.title}
+    </li>
+    );
+
+
+ return(
+    <div className="item">
+
+      <ul>{blogList}</ul>
+
+    </div>
+
+
+ );
+
+}
+
+export default OtherBlogs;
