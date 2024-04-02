c19d9c949cc404e63b9d2018e764091acaf23025
Kigen29 <emmanuel.kemboi@strathmore.edu>
2024-04-02 22:03:21 +0300

    feat: add Task 2 - alternative to passing props intro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Counter from './components/Counter';
 import Foodblog from './components/Foodblog';
+import Display from './components/Display';
 
 function App() {
   return (
     <div >
       <Foodblog/>
       <Counter/>
+      <Display/>
       
     </div>
   );
