3291c928fa44231b1a5faaeee19ad134244b5dc0
Kigen29 <emmanuel.kemboi@strathmore.edu>
2024-04-02 18:31:32 +0300

    feat: add Task 1- creating and nesting components

---
+++ src/components/Foodblog.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ProfileName from './ProfileName';
+
+function Foodblog() {
+  return (
+    <div>
+      <h1>My Food Blog</h1>
+      <ProfileName />
+      <p>My favorite food is pizza.</p>
+    </div>
+  );
+}
+
+export default Foodblog;
---
+++ src/components/ProfileName.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProfileName() {
+  return (
+    <div>
+    <h1>John Doe</h1>
+    </div>
+    );
+    
+}
+
+export default ProfileName;
